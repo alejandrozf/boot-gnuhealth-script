73cde33c65c9c1a61008b133c89fae65e91876fd
Alejandro Zamora Fonseca <allejjandrozf@gmail.com>
2019-05-30 00:11:06 -0400

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/do$
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 mkdir ~/gnuhealth
 cd gnuhealth
