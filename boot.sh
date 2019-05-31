baf0133cfe461968e253968707e3323a02822e3a
Alejandro Zamora Fonseca <allejjandrozf@gmail.com>
2019-05-30 22:26:20 -0400

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 mkdir ~/gnuhealth
-cd gnuhealth
+cd ~/gnuhealth
 curl -o ./docker-compose.yml https://raw.githubusercontent.com/mbehrle/docker-gnuhealth-demo/master/docker-compose.yml
 docker-compose up
